8cae7af88ed0dce6c59e98cd36b0c2b7d6c508e6
Jack Fraser <95544241+jackdfraser@users.noreply.github.com>
2026-04-17 10:05:18 -0500

    removing integration with neon auth and using better auth instead to maintain portability

--- service/database/auth.ts
+++ service/database/auth.ts
@@ -1,7 +1,7 @@
 import { betterAuth } from "better-auth";
 import { drizzleAdapter } from "better-auth/adapters/drizzle";
-import { drizzleDb } from "@/service/database";
-import * as schema from "@/migration/schema";
+import { drizzleDb } from "@/service/database/database";
+import * as schema from "@/service/database/schema";
 
 export const auth = betterAuth({
     database: drizzleAdapter(drizzleDb, {
